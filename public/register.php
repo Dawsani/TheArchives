368b7ec237e48a935c97b78cd57402d5beeab299
Dawson Matthews <dawsonmatthews@mines.edu>
2023-11-08 20:42:37 -0700

    pre styling and new gitignore

--- public/register.php
+++ public/register.php
@@ -1,31 +1,45 @@
+<?php
+include 'db_connection.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Register</title>
+    <title>The Archives</title>
 </head>
 <body>
 <main>
-    <form action="register.php" method="post">
-        <h1>Sign Up</h1>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username">
-        </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <label for="password2">Password Again:</label>
-            <input type="password" name="password2" id="password2">
-        </div>
-        <button type="submit">Register</button>
-        <footer>Already a member? <a href="login.php">Login here</a></footer>
-    </form>
-</main>
-</body>
-</html>
+
+<form action="register.php" method="post">
+    <h1>Sign Up</h1>
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" placeholder="Username">
+    </div>
+
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" value="<?= $inputs['email'] ?? '' ?>"
+               class="<?= error_class($errors, 'email') ?>">
+        <small><?= $errors['email'] ?? '' ?></small>
+    </div>
+
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" value="<?= $inputs['password'] ?? '' ?>"
+               class="<?= error_class($errors, 'password') ?>">
+        <small><?= $errors['password'] ?? '' ?></small>
+    </div>
+
+    <div>
+        <label for="password2">Password Again:</label>
+        <input type="password" name="password2" id="password2" value="<?= $inputs['password2'] ?? '' ?>"
+               class="<?= error_class($errors, 'password2') ?>">
+        <small><?= $errors['password2'] ?? '' ?></small>
+    </div>
+
+    <button type="submit">Register</button>
+
+    <footer>Already a member? <a href="login.php">Login here</a></footer>
+
+</form>
--- src/inc/header.php
+++ src/inc/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Register</title>
+    <title><?= $title ?? 'Home' ?></title>
 </head>
 <body>
 <main>
