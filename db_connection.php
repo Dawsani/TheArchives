0c9ab7658ee59b06045dd70b2392e94ef33fea3d
Dawson Matthews <dawson.dwm@gmail.com>
2023-11-29 11:25:30 -0700

    added password back

--- db_connection.php
+++ db_connection.php
@@ -29,7 +29,7 @@ else {
 
 $host = "localhost";
 $username = "root";
-$user_pass = "root";
+$user_pass = "b5Aw!36Fk%4zVU4N";
 $database_in_use = "clips_schema";
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
